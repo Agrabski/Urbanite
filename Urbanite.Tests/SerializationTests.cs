9938341cb46b874877c989ae8a7e3e1eb50a6d5d
Adam Grabski <adam.gr@outlook.com>
2023-08-24 12:36:39 +0200

    added test for non generic api

--- Urbanite.Tests/SerializationTests.cs
+++ Urbanite.Tests/SerializationTests.cs
@@ -22,4 +22,23 @@ public class SerializationTests
 		Assert.Equal("""[{"$type":"a","PropertyA":0},{"$type":"b","PropertyB":null}]""", text);
 
 	}
+
+
+	[Fact]
+	public void ListOfPolymorphicTypesCanBeSerializedWhenRegisteredUsingNonGenericApi()
+	{
+		var serviceProvider = new ServiceCollection()
+			.AddUrbanite()
+			.AddUrbaniteSerializableType(typeof(IBase), typeof(A), "a")
+			.AddUrbaniteSerializableType(typeof(IBase), typeof(B), "b")
+			.BuildServiceProvider()
+			;
+		var options = new JsonSerializerOptions()
+		{
+			TypeInfoResolver = serviceProvider.GetRequiredService<IPolymorphicTypeResolver>()
+		};
+		var text = JsonSerializer.Serialize(new List<IBase>() { new A(), new B() }, options);
+		Assert.Equal("""[{"$type":"a","PropertyA":0},{"$type":"b","PropertyB":null}]""", text);
+
+	}
 }
